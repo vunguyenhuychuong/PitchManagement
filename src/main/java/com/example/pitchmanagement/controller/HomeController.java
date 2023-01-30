6c8243a1563aae13988907403b8c254d239c055c
TrungNT <nguyentantrung1801@gmail.com>
2023-01-30 11:16:57 +0700

    update controller for pagination.

--- src/main/java/com/example/pitchmanagement/controller/HomeController.java
+++ src/main/java/com/example/pitchmanagement/controller/HomeController.java
@@ -1,13 +1,41 @@
 package com.example.pitchmanagement.controller;
 
+import com.example.pitchmanagement.entity.Pitch;
+import com.example.pitchmanagement.service.PitchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PitchService service;
     @RequestMapping(value = {"/home", "/"})
-    public String HomePage() {
+    public String HomePage(Model model,
+                           @RequestParam("page")Optional<Integer> page,
+                           @RequestParam("size")Optional<Integer> size) {
+        //-----------------------------------
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(8);
+        Page<Pitch> pitchPage = service.paginatedPitch(PageRequest.of(currentPage-1, pageSize));
+        model.addAttribute("pitchPage", pitchPage);
+        int totalPages = pitchPage.getTotalPages();
+        if(totalPages > 0){
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        //-----------------------------------
         return "home/index.html";
     }
+
 }
